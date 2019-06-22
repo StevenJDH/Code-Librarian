be7864c5c45faf53bac30b98569d40d9e497360d
Steven Jenkins De Haro <stevendeharo@gmail.com>
2019-06-22 12:08:22 +0200

    Fixed incorrect startup position

--- Code Librarian/FrmAbout.designer.cs
+++ Code Librarian/FrmAbout.designer.cs
@@ -177,7 +177,7 @@
             this.Name = "FrmAbout";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "About Code Librarian";
             this.Load += new System.EventHandler(this.FrmAbout_Load);
             this.groupBox1.ResumeLayout(false);
