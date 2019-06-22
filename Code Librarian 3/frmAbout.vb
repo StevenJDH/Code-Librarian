331fe701f28ab04c7cb6f0216d1e561ddee7fd4b
Steven Jenkins De Haro <stevendeharo@gmail.com>
2019-06-22 13:05:00 +0200

    Replaced VB.Net project with C# project

--- Code Librarian 3/frmAbout.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class frmAbout
-
-    Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
-        Me.Close()
-    End Sub
-
-End Class
